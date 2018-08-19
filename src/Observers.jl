c842202c4d179d90549518b2be9ac12ca7d8902e
Ritchie Lee <ritchie.lee@gmail.com>
2018-08-19 10:21:42 -0700

    initial

--- src/Observers.jl
+++ src/Observers.jl
@@ -1,5 +1,11 @@
 module Observers
 
-# package code goes here
+export Observer, notify_observer!
+
+abstract type Observer end
+
+# Users should override this
+notify_observer!(o::Observer, x; kwargs...) = x
+notify_observer!(o::Void, x; kwargs...) = x
 
 end # module
