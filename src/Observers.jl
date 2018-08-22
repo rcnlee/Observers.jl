84cb5b94c602db52ae44992bbdbfaa1d08afd657
Ritchie Lee <ritchie.lee@gmail.com>
2018-08-21 18:56:23 -0700

    added kwargs_get

--- src/Observers.jl
+++ src/Observers.jl
@@ -1,11 +1,17 @@
 module Observers
 
-export Observer, notify_observer!
+export Observer, notify_observer!, kwargs_get
 
 abstract type Observer end
 
 # Users should override this
 notify_observer!(o::Observer, x; kwargs...) = x
 notify_observer!(o::Void, x; kwargs...) = x
+
+function kwargs_get(kwargs::Vector, key::Symbol)
+    i = findfirst(x->x[1]==key, kwargs)
+    i == 0 && error("key $key not found")
+    kwargs[i][2]
+end
 
 end # module
